1125dfdaea341b28a9d478f735e70d49a5f39454
emhedge <122066271+emhedge@users.noreply.github.com>
2026-02-06 17:15:01 +0800

    Added book prototype function, revised constructor
    I added a prototype function to toggle the read
    status of the book (incomplete) and revised the
    event listener for bookDialog to create a
    newBookInstance(allValues), which is then passed
    on to addBookToLibrary and addBookCard. This
    ensures that the new book ID is created according
    to my new Book() constructor.
    
    Also revised myLibrary to be an empty array at the
    start, created initialBooks which calls new Book()
    on each object, and added a line to the for loop
    setting my initial text content that adds the
    generated id for each object to each bookCard's
    dataset.

--- script.js
+++ script.js
@@ -1,49 +1,14 @@
-const myLibrary = [
-    {title: "Slaughterhouse Five", author: "Kurt Vonnegut, Jr.", pages: "190 pages", read:"read", id: "1"},
-    {title: "Leviathan Wakes", author: "James S.A. Corey", pages: "561 pages", read:"read", id: "2"},
-    {title: "Project Hail Mary", author: "Andy Weir", pages: "496 pages", read:"not read yet", id: "3"},
-];
-
 const cardContainer = document.querySelector("#card-container")
-let bookCard = document.querySelectorAll(".book-card");
-let bookTitle = document.querySelectorAll(".book-title");
-let bookAuthor = document.querySelectorAll(".author");
-let bookPages = document.querySelectorAll(".pages");
-let bookRead = document.querySelectorAll(".read");
+const bookCard = document.querySelectorAll(".book-card");
+const bookTitle = document.querySelectorAll(".book-title");
+const bookAuthor = document.querySelectorAll(".author");
+const bookPages = document.querySelectorAll(".pages");
+const bookRead = document.querySelectorAll(".read");
 const newBookBtn = document.querySelector("#add-new-btn")
 const dialog = document.querySelector('#bookDialog');
 const openBtn = document.querySelector('#openModal');
 const closeBtn = document.querySelector('#closeModal');
-
-// set initial text content
-for (let i = 0; i < myLibrary.length; i++) {
-    bookTitle[i].textContent = myLibrary[i].title;
-    bookAuthor[i].textContent = myLibrary[i].author;
-    bookPages[i].textContent = myLibrary[i].pages;
-    bookRead[i].textContent = myLibrary[i].read;
-}
-
-// Show the pop-up
-openBtn.addEventListener('click', () => {
-  dialog.showModal(); 
-});
-
-// Close the pop-up
-closeBtn.addEventListener('click', () => {
-  dialog.close();
-});
-
-// get form data, call addBookToLibrary()
-const bookDialog = document.querySelector("#bookDialog");
-
-bookDialog.addEventListener("submit", e => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const allValues = Object.fromEntries(formData.entries());
-    new Book(allValues);
-    console.log(allValues)
-    dialog.close();
-})
+const bookRemovalBtn = document.querySelectorAll(".book-card-btn");
 
 // new Book constructor
 function Book({title, author, pages, read}) {
@@ -58,17 +23,108 @@ function Book({title, author, pages, read}) {
     this.info = function() {
         return `${title} by ${author}, ${pages}, ${read}`;    
     }
-    addBookToLibrary(this);
-    console.log(this)
 }
-// add book to library and create new .book-card for it
-function addBookToLibrary(object) {
-    
-    myLibrary.push(object);
-    
 
+// set prototype for Book
+Book.prototype.toggleRead = function() {
+    this.read = this.read == "read" ? "not read yet" : "read";
+    
+}
+
+myLibrary = [];
+
+const initialBooks = [
+    new Book({title: "Slaughterhouse Five", author: "Kurt Vonnegut, Jr.", pages: "190 pages", read:"read", id: ""}),
+    new Book({title: "Leviathan Wakes", author: "James S.A. Corey", pages: "561 pages", read:"read", id: ""}),
+    new Book({title: "Project Hail Mary", author: "Andy Weir", pages: "496 pages", read:"not read yet", id: ""}),
+];
+
+initialBooks.forEach(book => myLibrary.push(book));
+
+// add books to library and create new .book-card for it
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    console.table(myLibrary);
+}
+
+
+
+// set initial text content
+for (let i = 0; i < myLibrary.length; i++) {
+    bookTitle[i].textContent = myLibrary[i].title;
+    bookAuthor[i].textContent = myLibrary[i].author;
+    bookPages[i].textContent = myLibrary[i].pages;
+    bookRead[i].textContent = myLibrary[i].read;
+    bookCard[i].dataset.id = myLibrary[i].id;
+}
+
+// event listeners
+// Show the pop-up
+openBtn.addEventListener('click', () => {
+    dialog.showModal(); 
+});
+
+// Close the pop-up
+closeBtn.addEventListener('click', () => {
+    dialog.close();
+});
+
+// Remove book card
+bookRemovalBtn.forEach(button => {
+    button.addEventListener('click', removeFromLibrary)
+    button.addEventListener('click', removeBookCard)
+})
+
+
+// get form data, call addBookToLibrary()
+const bookDialog = document.querySelector("#bookDialog");
+
+bookDialog.addEventListener("submit", e => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const allValues = Object.fromEntries(formData.entries());
+
+    const newBookInstance = new Book(allValues);
+
+    addBookToLibrary(newBookInstance);
+    addBookCard(newBookInstance);
+
+    dialog.close();
+    e.target.reset();
+})
+
+// toggle read status
+const readButton = document.querySelector(".toggle-read-btn");
+const notRead = "not read yet";
+const read = "read";
+
+cardContainer.addEventListener("click", e => {
+    
+    
+    e.preventDefault();
+    if (e.target.classList.contains("toggle-read-btn")) {
+        const button = e.target;
+        const thisCard = button.closest(".book-card")
+        // const currentStatus = thisCard.querySelector(".read");
+        const bookId = thisCard.dataset.id;
+
+        const foundBook = myLibrary.find(book => book.id == bookId)
+
+        console.log(foundBook);
+        
+        // if (currentStatus.textContent == read) {
+        //     currentStatus.textContent = notRead;
+        // } else {
+        //     currentStatus.textContent = read;
+        // }
+        foundBook.toggleRead();
+    }
+    
+})
+
+function addBookCard(object) {
         // check if new book is added
-    if (bookCard.length != myLibrary.length) {
+
         // add card div with title, author, pages, read divs
         const newCard = document.createElement("div");
         const newTitle = document.createElement("div");
@@ -76,21 +132,24 @@ function addBookToLibrary(object) {
         const newAuthor = document.createElement("p");
         const newPages = document.createElement("p");
         const newRead = document.createElement("p");
-
+        const newBookRemovalBtn = document.createElement("button");
         // set class
         newCard.setAttribute("class", "book-card");
+        newCard.dataset.id = object.id;
         newInfo.setAttribute("class", "book-info");
         newTitle.setAttribute("class", "book-title");
         newAuthor.setAttribute("class", "book-author");
         newPages.setAttribute("class", "book-pages");
         newRead.setAttribute("class", "book-read");
 
+        newBookRemovalBtn.setAttribute("class", "book-card-btn")
+        newBookRemovalBtn.textContent = "Remove from library";
+
         // set text content
         newTitle.textContent = object.title;
         newAuthor.textContent = object.author;
         newPages.textContent = object.pages;
         newRead.textContent = object.read;
-        console.log(object.title)
 
         // append
         cardContainer.appendChild(newCard);
@@ -99,11 +158,30 @@ function addBookToLibrary(object) {
         newInfo.appendChild(newAuthor);
         newInfo.appendChild(newPages);
         newInfo.appendChild(newRead);
+        newCard.appendChild(newBookRemovalBtn);
+    // console.log(object.pages)
+}
+
+function removeBookCard(event) {
+    const button = event.target;
+    const bookCardDiv = button.closest(".book-card");
+    if (bookCardDiv) {
+        const bookId = bookCardDiv.getAttribute("data-id");
+        console.log("Removing item with ID:", bookId);
     }
-    console.log(object.pages)
-
+    bookCardDiv.remove()
 }
 
-function removeFromLibrary(object) {
-    myLibrary.slice(object);
+
+function removeFromLibrary(event) {
+    const button = event.target;
+    const bookCardDiv = button.closest(".book-card");
+    const bookId = bookCardDiv.getAttribute("data-id");
+
+    // findIndex to identify location with same id
+    const bookIndex = myLibrary.findIndex(book => book.id == bookId);
+    // splice to remove that object in that location
+    myLibrary.splice(bookIndex, 1);
+    console.table(myLibrary)
 }
+
