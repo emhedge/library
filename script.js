fbabd4681c4d0cbf381902e473ad6e2209541917
emhedge <122066271+emhedge@users.noreply.github.com>
2026-02-10 11:02:15 +0800

    Removed static declarations and set btn toggle
    On page load, the three default cards were hard-
    coded in html and populated with a for loop. This
    static approach is fragile, so I updated the code
    to dynamically populate book card/content via the
    addBookCard(object) function.
    
    I also added a classList.toggle declaration in the
     event listener to update the toggle-read-btn's
     background color based on the content of
     foundBook.read.

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 const cardContainer = document.querySelector("#card-container")
-const bookCard = document.querySelectorAll(".book-card");
-const bookTitle = document.querySelectorAll(".book-title");
-const bookAuthor = document.querySelectorAll(".author-specifics");
-const bookPages = document.querySelectorAll(".pages-specifics");
-const bookRead = document.querySelectorAll(".read-specifics");
 const newBookBtn = document.querySelector("#add-new-btn")
 const dialog = document.querySelector('#bookDialog');
 const openBtn = document.querySelector('#openModal');
 const closeBtn = document.querySelector('#closeModal');
-const bookRemovalBtn = document.querySelectorAll(".book-removal-btn");
 
 // new Book constructor
 function Book({title, author, pages, read}) {
@@ -27,36 +21,26 @@ function Book({title, author, pages, read}) {
 
 // set prototype for Book
 Book.prototype.toggleRead = function() {
-    this.read = this.read == "read" ? "not read yet" : "read";
-    
+    this.read = this.read == "Read" ? "Not yet read" : "Read";
 }
-
 myLibrary = [];
 
 // start with three books
 const initialBooks = [
     new Book({title: "Slaughterhouse Five", author: "Kurt Vonnegut, Jr.", pages: "190 pages", read:"Read"}),
     new Book({title: "Leviathan Wakes", author: "James S.A. Corey", pages: "561 pages", read:"Read"}),
-    new Book({title: "Project Hail Mary", author: "Andy Weir", pages: "496 pages", read:"Read"}),
+    new Book({title: "Project Hail Mary", author: "Andy Weir", pages: "496 pages", read:"Not yet read"}),
 ];
 
-initialBooks.forEach(book => myLibrary.push(book));
+initialBooks.forEach(book => {
+    addBookToLibrary(book);
+    addBookCard(book);
+});
 
 // add books to library and create new .book-card for it
 function addBookToLibrary(book) {
     myLibrary.push(book);
     console.table(myLibrary);
-}
-
-
-
-// set initial text content
-for (let i = 0; i < myLibrary.length; i++) {
-    bookTitle[i].textContent = myLibrary[i].title;
-    bookAuthor[i].textContent = myLibrary[i].author;
-    bookPages[i].textContent = myLibrary[i].pages;
-    bookRead[i].textContent = myLibrary[i].read;
-    bookCard[i].dataset.id = myLibrary[i].id;
 }
 
 // event listeners
@@ -72,7 +56,7 @@ closeBtn.addEventListener('click', () => {
 
 
 // event handlers for toggling read and removing from library
-const notRead = "Not read yet";
+const notRead = "Not yet read";
 const read = "Read";
 
 cardContainer.addEventListener("click", e => {
@@ -83,14 +67,17 @@ cardContainer.addEventListener("click", e => {
         const currentStatus = thisCard.querySelector(".read-specifics");
         const bookId = thisCard.dataset.id;
 
-        const foundBook = myLibrary.find(book => book.id == bookId)
+        const foundBook = myLibrary.find(book => book.id == bookId);
         
+        
+
         if (currentStatus.textContent == read) {
             currentStatus.textContent = notRead;
         } else {
             currentStatus.textContent = read;
         }
         foundBook.toggleRead();
+        button.classList.toggle("is-read", foundBook.read === "Read");
     }
 
     if (e.target.classList.contains("book-removal-btn")) {
@@ -122,51 +109,71 @@ bookDialog.addEventListener("submit", e => {
 })
 
 function addBookCard(object) {
-        // add card div with title, author, pages, read divs
-        const newCard = document.createElement("div");
-        const newTitle = document.createElement("div");
-        const newInfo = document.createElement("div");
-        const newBookSpecifics = document.createElement("div");
-        const newAuthor = document.createElement("p");
-        const newPages = document.createElement("p");
-        const newRead = document.createElement("p");
-        const newBtnDiv = document.createElement("div");
-        const newBookRemovalBtn = document.createElement("button");
-        const newReadBtn = document.createElement("button");
+    // add card div with title, author, pages, read divs
+    const newCard = document.createElement("div");
+    const newTitle = document.createElement("div");
+    const newInfo = document.createElement("div");
+    const newBookDescriptors = document.createElement("div");
+    const newBookSpecifics = document.createElement("div");
+    const newAuthor = document.createElement("p");
+    const newAuthorSpecs = document.createElement("p");
+    const newPages = document.createElement("p");
+    const newPagesSpecs = document.createElement("p");
+    const newRead = document.createElement("p");
+    const newReadSpecs = document.createElement("p");
+    const newBtnDiv = document.createElement("div");
+    const newBookRemovalBtn = document.createElement("button");
+    const newReadBtn = document.createElement("button");
+    
 
-        // set class
-        newCard.setAttribute("class", "book-card");
-        newCard.dataset.id = object.id;
-        newInfo.setAttribute("class", "book-info");
-        newBookSpecifics.setAttribute("id", "book-specifics")
-        newTitle.setAttribute("class", "book-title");
-        newAuthor.setAttribute("class", "author-specifics");
-        newPages.setAttribute("class", "pages-specifics");
-        newRead.setAttribute("class", "read-specifics");
-        newBtnDiv.setAttribute("class", "book-card-btn-div");
-        newBookRemovalBtn.setAttribute("class", "book-removal-btn")
-        newBookRemovalBtn.setAttribute("type", "button")
-        newBookRemovalBtn.textContent = "Remove from library";
-        newReadBtn.setAttribute("class", "toggle-read-btn");
-        newReadBtn.setAttribute("type", "button")
-        newReadBtn.textContent = "Toggle read";
+    // set class
+    newCard.setAttribute("class", "book-card");
+    newCard.dataset.id = object.id;
+    newInfo.setAttribute("class", "book-info");
+    newBookDescriptors.setAttribute("id", "book-descriptors");
+    newBookSpecifics.setAttribute("id", "book-specifics")
+    newTitle.setAttribute("class", "book-title");
+    newAuthor.setAttribute("class", "author");
+    newAuthor.textContent = "Author:"
+    newAuthorSpecs.setAttribute("class", "author-specifics");
+    newPages.setAttribute("class", "pages");
+    newPages.textContent = "Page Count:"
+    newPagesSpecs.setAttribute("class", "pages-specifics");
+    newRead.setAttribute("class", "read");
+    newRead.textContent = "Finished?"
+    newReadSpecs.setAttribute("class", "read-specifics");
+    newBtnDiv.setAttribute("class", "book-card-btn-div");
+    newBookRemovalBtn.setAttribute("class", "book-removal-btn")
+    newBookRemovalBtn.setAttribute("type", "button")
+    newBookRemovalBtn.textContent = "Remove from library";
+    newReadBtn.setAttribute("class", "toggle-read-btn");
+    newReadBtn.setAttribute("type", "button")
+    newReadBtn.textContent = "Finished";
 
-        // set text content
-        newTitle.textContent = object.title;
-        newAuthor.textContent = object.author;
-        newPages.textContent = object.pages;
-        newRead.textContent = object.read;
+    // set text content
+    newTitle.textContent = object.title;
+    newAuthorSpecs.textContent = object.author;
+    newPagesSpecs.textContent = object.pages;
+    newReadSpecs.textContent = object.read;
 
-        // append
-        cardContainer.appendChild(newCard);
-        newCard.appendChild(newTitle);
-        newCard.appendChild(newInfo);
-        newInfo.appendChild(newBookSpecifics);
-        newBookSpecifics.appendChild(newAuthor);
-        newBookSpecifics.appendChild(newPages);
-        newBookSpecifics.appendChild(newRead);
-        
-        newCard.appendChild(newBtnDiv);
-        newBtnDiv.appendChild(newBookRemovalBtn);
-        newBtnDiv.appendChild(newReadBtn);
+    // append
+    cardContainer.appendChild(newCard);
+    newCard.appendChild(newTitle);
+    newCard.appendChild(newInfo);
+    newInfo.appendChild(newBookDescriptors);
+    newInfo.appendChild(newBookSpecifics);
+    newBookDescriptors.appendChild(newAuthor);
+    newBookSpecifics.appendChild(newAuthorSpecs);
+    newBookDescriptors.appendChild(newPages);
+    newBookSpecifics.appendChild(newPagesSpecs);
+    newBookDescriptors.appendChild(newRead);
+    newBookSpecifics.appendChild(newReadSpecs);
+    
+    newCard.appendChild(newBtnDiv);
+    newBtnDiv.appendChild(newReadBtn);
+    newBtnDiv.appendChild(newBookRemovalBtn);
+
+    if (object.read === "Read") {
+        newReadBtn.classList.add("is-read")
+    }
 }
